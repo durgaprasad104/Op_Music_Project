71d3b6fc3f50e33b4d9e5f89d73d20b2b5a4d3ab
Durgaprasad <128016655+durgaprasad104@users.noreply.github.com>
2026-03-18 09:51:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://op-music-project.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
